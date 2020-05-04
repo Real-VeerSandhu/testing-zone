b7d2e0b793de294b6753eae834de230d73b97ef0
Real-VeerSandhu <sandhu.s.veer@gmail.com>
2020-05-04 16:07:12 -0400

    Start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import {MatButtonModule} from '@angular/material/button';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { CheckComponent } from './check/check.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule, MatButtonModule ],
+  declarations: [ AppComponent, HelloComponent, CheckComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
